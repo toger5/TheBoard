0f578a740f0186e7e73a2d02a8ab8141a444fefd
Timo k <toger5@hotmail.de>
2021-08-05 01:40:15 +0200

    make vs code debugger work

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,13 +2,14 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const path = require('path');
 let distName = 'distDev'
-module.exports = merge(common,{
+module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    watchContentBase: true
+    watchContentBase: true,
+    writeToDisk: true
   },
   output: {
     path: path.resolve(__dirname, distName),
-},
+  },
 })
